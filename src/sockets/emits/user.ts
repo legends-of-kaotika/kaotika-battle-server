ac9efe716e5a2c7ed8632d6f968fba21f0476277
Unai <reikrb@gmail.com>
2025-01-27 10:45:14 +0100

    [KBS-25][UPDATE] Socket emit users method name

--- src/sockets/emits/user.ts
+++ src/sockets/emits/user.ts
@@ -2,7 +2,9 @@ import { Server, Socket } from "socket.io";
 import { ONLINE_USERS } from "../../game";
 
 //sends an array with the connected users to web client on user connection
-export const sendConnectedUsersArrayToWeb = (io: Server, socket: Socket):void => {
-    socket.emit("connected-users", ONLINE_USERS);
+export const sendConnectedUsersArray = (io: Server):void => {
+    io.emit("connected-users", ONLINE_USERS);
 }
+
+
 
--- src/sockets/listeners/webHandlers/user.ts
+++ src/sockets/listeners/webHandlers/user.ts
@@ -1,11 +1,11 @@
 import { Server, Socket } from "socket.io";
-import { sendConnectedUsersArrayToWeb } from "../../emits/user";
+import { sendConnectedUsersArray } from "../../emits/user";
 
 module.exports = (io: Server, socket: Socket) => {
   socket.on("Web-UserTest", async () => {
     socket.emit("Web-UserTest", "User for Web");
   });
-  socket.on("Web-send-dudes", async () => {
-    sendConnectedUsersArrayToWeb(io, socket);
+  socket.on("Web-sendUsers", async () => {
+    sendConnectedUsersArray(io);
   });
 };
