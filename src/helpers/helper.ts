b4dece11bddee582dc86f2225a08fd98d7f3d8b4
LarreaE <eneko.larrea@ikasle.aeg.eus>
2025-01-27 12:49:09 +0100

    [KBS-32][ADD] findPlayerById

---
+++ src/helpers/helper.ts
@@ -0,0 +1,12 @@
+import { ONLINE_USERS } from "../game";
+import { Player } from "../interfaces/Player";
+
+//sends an array with the connected users to web client on user connection
+export const findPlayerById = (_id: string): Player | string => {
+  for (let i = 0; i < ONLINE_USERS.length; i++) {
+    if (ONLINE_USERS[i]._id === _id) {
+      return ONLINE_USERS[i];
+    }
+  }
+  return 'No players found'
+};
