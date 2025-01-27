22ea0ffb8e8d671fbc1e285843f29efacb3d0f00
Unai <reikrb@gmail.com>
2025-01-27 10:46:00 +0100

    [KBS-25][ADD] Mobile-GameStart socket listener

--- src/sockets/listeners/mobileHandlers/user.ts
+++ src/sockets/listeners/mobileHandlers/user.ts
@@ -1,8 +1,13 @@
 import { Server, Socket } from "socket.io";
+import { sendConnectedUsersArray } from "../../emits/user";
 
 module.exports = (io: Server, socket: Socket) => {
 
   socket.on("Mobile-UserTest", async () => {
     socket.emit("Mobile-UserTest", 'User for Mobile');
   });
+
+  socket.on('Mobile-GameStart', async () => {
+    sendConnectedUsersArray(io)
+  })
 }
