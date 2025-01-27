52e7f97014e372e7189f6b477de4170d5446a3c4
victor06Duran <victor.duran@ikasle.aeg.eus>
2025-01-27 12:07:59 +0100

    [KBS-23][ADD]The service for the route GET to retrieve the player data

---
+++ src/services/playerService.ts
@@ -0,0 +1,95 @@
+const Player = require("../models/playerSchema");
+import {Player} from "../interfaces/Player"; 
+
+const filterPlayerData = (data: any): Player => {
+  return {
+    _id: data._id,
+    name: data.name || '',
+    nickname: data.nickname || '',
+    avatar: data.avatar || '',
+    email: data.email || '',
+    level: data.level || 0,
+    role: data.role || '',
+    deviceToken: data.deviceToken || '',
+    profile: data.profile
+      ? {
+        name: data.profile.name || '',
+      }
+      : null,
+    attributes: data.attributes || {
+      intelligence: 0,
+      dexterity: 0,
+      insanity: 0,
+      charisma: 0,
+      constitution: 0,
+      strength: 0,
+    },
+    equipment: {
+      helmet: data.equipment?.helmet || null,
+      weapon: data.equipment?.weapon || {},
+      armor: data.equipment?.armor || {},
+      shield: data.equipment?.shield || null,
+      artifact: data.equipment?.artifact || {},
+      boot: data.equipment?.boot || null,
+      ring: data.equipment?.ring || null,
+      healing_potion: data.equipment?.healing_potion || {},
+      antidote_potion: data.equipment?.antidote_potion || {},
+      enhancer_potion: data.equipment?.enhancer_potion || {},
+    },
+    inventory: {
+      healing_potions: data.inventory?.healing_potions || [],
+      antidote_potions: data.inventory?.antidote_potions || [],
+      enhancer_potions: data.inventory?.enhancer_potions || [],
+    },
+  };
+};
+
+const assignRole = (email: String) => {
+  switch (email) {
+    case process.env.ISTVAN_EMAIL:
+      return 'istvan'
+    case process.env.VILLAIN_EMAIL:
+      return 'villain'
+    case process.env.MORTIMER_EMAIL:
+      return 'mortimer'
+    default:
+      return 'acolyte'
+  }
+}
+const initFetchPlayer = async (email: String) => {
+  try {
+    const queryResponse = await fetch(`https://kaotika-server.fly.dev/players/email/${email}/`);
+    const userData = await queryResponse.json();
+    const role = assignRole(userData.data.email);
+
+    let user = filterPlayerData(userData.data);
+    user.role = role;
+
+    console.log('Email: ', email);
+    console.log('Role: ', role);
+
+    const existingPlayer = await Player.findOne({ email: user.email });
+
+    let player;
+    if (!existingPlayer) {
+      // ADD TO COLLECTION
+      player = new Player(user);
+      await player.save();
+      console.log('Added new player: ', player);
+    } else {
+      // MERGE TO COLLECTION
+      Object.assign(existingPlayer, user);
+      await existingPlayer.save();
+      player = existingPlayer;
+    }
+
+    return player;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+
+module.exports = { initFetchPlayer };
+
