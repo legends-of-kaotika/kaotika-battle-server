96d05f0064658b872cedf8c9dba09c2e7f2d2866
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-01-27 22:29:22 +0100

    [ADD] New socket.on when players on mobile selects an action and the emits for the web

--- src/sockets/emits/user.ts
+++ src/sockets/emits/user.ts
@@ -7,4 +7,23 @@ export const sendConnectedUsersArray = (io: Server):void => {
 }
 
 
+// Sends tho the web that tha actual turn player selected to attack
+export const sendAttackSelectedToWeb = (io: Server):void => {
+    io.emit("web-attackSelected");
+}
+
+// Sends tho the web that tha actual turn player selected to heal
+export const sendHealSelectedToWeb = (io: Server):void => {
+    io.emit("web-healSelected");
+}
+
+// Sends tho the web that tha actual turn player selected to heal
+export const sendCurseSelectedToWeb = (io: Server):void => {
+    io.emit("web-curseSelected");
+}
+
+// Sends tho the web that tha actual turn player selected to use a potion
+export const sendUsePotionSelectedToWeb = (io: Server):void => {
+    io.emit("web-usePotionSelected");
+}
 
