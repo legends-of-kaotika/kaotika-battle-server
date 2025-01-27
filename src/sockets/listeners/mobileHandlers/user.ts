e8703a081cfd2583cb5fd656d64b75e9e041607e
LarreaE <eneko.larrea@ikasle.aeg.eus>
2025-01-27 12:22:45 +0100

    [KBS-31][ADD] attack socket structure without damage calculations.

--- src/sockets/listeners/mobileHandlers/user.ts
+++ src/sockets/listeners/mobileHandlers/user.ts
@@ -1,5 +1,6 @@
 import { Server, Socket } from "socket.io";
 import { sendConnectedUsersArray } from "../../emits/user";
+import { ONLINE_USERS } from "../../../game";
 
 module.exports = (io: Server, socket: Socket) => {
 
@@ -10,4 +11,26 @@ module.exports = (io: Server, socket: Socket) => {
   socket.on('mobile-gameStart', async () => {
     sendConnectedUsersArray(io)
   })
+
+  socket.on('mobile-attack', async (data) => {
+    const { _id } = data;
+    let attacker;
+    let defender;
+    for (let i = 0; i < ONLINE_USERS.length; i++) {
+      if (ONLINE_USERS[i].socketId === socket.id) {
+        attacker = ONLINE_USERS[i];
+      }
+      if (ONLINE_USERS[i]._id === _id) {
+        defender = ONLINE_USERS[i];
+      } 
+    } 
+
+    //calculate damage
+    let totalDmg = 0;
+
+    
+    //return players to web
+    sendConnectedUsersArray(io)
+  })
+  
 }
