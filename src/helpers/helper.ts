3a7d7ecfd81cb6fbb4179f8f18f3453e1e4e31ce
LarreaE <eneko.larrea@ikasle.aeg.eus>
2025-01-28 08:35:42 +0100

    [KBS][ADD] fincPlayerByEmail

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,23 +1,32 @@
 import { ONLINE_USERS } from "../game";
 import { Player } from "../interfaces/Player";
 
-//sends an array with the connected users to web client on user connection
+//returns a player searched by id
 export const findPlayerById = (_id: string): Player | string => {
   for (let i = 0; i < ONLINE_USERS.length; i++) {
     if (ONLINE_USERS[i]._id === _id) {
       return ONLINE_USERS[i];
     }
   }
-  return 'No players found'
+  return "No players found";
 };
 
-//sends an array with the connected users to web client on user connection
+//returns a player searched by socketid
 export const findPlayerBySocketId = (id: string): Player | string => {
-    for (let i = 0; i < ONLINE_USERS.length; i++) {
-      if (ONLINE_USERS[i].socketId === id) {
-        return ONLINE_USERS[i];
-      }
+  for (let i = 0; i < ONLINE_USERS.length; i++) {
+    if (ONLINE_USERS[i].socketId === id) {
+      return ONLINE_USERS[i];
     }
-    return 'No players found'
-  };
-  
+  }
+  return "No players found";
+};
+
+//returns a player searched by email
+export const findPlayerByEmail = (email: string): Player | string => {
+  for (let i = 0; i < ONLINE_USERS.length; i++) {
+    if (ONLINE_USERS[i].email === email) {
+      return ONLINE_USERS[i];
+    }
+  }
+  return "No players found";
+};
