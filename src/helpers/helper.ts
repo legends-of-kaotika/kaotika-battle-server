ddfd92b63a9d8db3ad57b14272ef29224e163eab
aitor <aitor.mendiburu@ikasle.aeg.eus>
2025-01-28 09:22:25 +0100

    [ADD]websocket listenr for sendSocketId

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -20,4 +20,24 @@ export const findPlayerBySocketId = (id: string): Player | string => {
     }
     return 'No players found'
   };
+
+//inserts socketId in the specific player of playerConnected[] global variable
+export const insertSocketId = (email: string, socketId: string): Player | undefined => {
+  const user = ONLINE_USERS.find((user)=> user.email === email);
+ if (user) {
+  user.socketId = socketId;
+  return user;
+ }
+ return undefined;
+}
+
+//removes the player that got disconnected from playerConnected[] global variable
+export const removePlayerConnected = (socketId: string): void => {
+  const userIndex = ONLINE_USERS.findIndex((user)=> user.socketId === socketId);
+  if (userIndex != -1) {
+    ONLINE_USERS.splice(userIndex, 1);
+  }
+}
+
+
   
--- src/sockets/listeners/mobileHandlers/user.ts
+++ src/sockets/listeners/mobileHandlers/user.ts
@@ -1,13 +1,31 @@
 import { Server, Socket } from "socket.io";
 import { sendConnectedUsersArray } from "../../emits/user";
 import { ONLINE_USERS } from "../../../game";
-import { findPlayerById, findPlayerBySocketId } from "../../../helpers/helper";
+import { findPlayerById, findPlayerBySocketId, insertSocketId, removePlayerConnected } from "../../../helpers/helper";
 
 module.exports = (io: Server, socket: Socket) => {
 
   socket.on("mobile-userTest", async () => {
     socket.emit("mobile-userTest", 'User for mobile');
   });
+
+  //////////////////////////////////////////////////
+  //receive socketId + email from clientMobile
+  socket.on("mobile-sendSocketId", async (email: string) => {
+    console.log(`new player with socketId: ${socket.id} ${email}`);
+    const newPlayerConnected = insertSocketId(email, socket.id);
+    //send to web the new player connected
+  })
+  ///////////////////////////////////////////////////
+  // //when player disconnects
+  // socket.on("disconnect", async () => {
+  //   console.log(`player with socketId ${socket.id} disconnected`);
+  //   removePlayerConnected(socket.id);
+  //   console.log("quitado victor: " + JSON.stringify(ONLINE_USERS));
+  //   socket.broadcast.emit("playerDisconnected", {playerSocketId: socket.id});
+  // })
+
+  ////////////////////////////////////////////////////
 
   socket.on('mobile-gameStart', async () => {
     sendConnectedUsersArray(io)
