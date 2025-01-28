613109070887865f9132cefe3729dd3e23ee63f2
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-01-28 09:42:29 +0100

    [KBS-40][ADD] The socket emit for user to web

--- src/sockets/emits/user.ts
+++ src/sockets/emits/user.ts
@@ -1,11 +1,11 @@
 import { Server, Socket } from "socket.io";
 import { ONLINE_USERS } from "../../game";
+import { Player } from "../../interfaces/Player";
 
 //sends an array with the connected users to web client on user connection
 export const sendConnectedUsersArray = (io: Server):void => {
     io.emit("connectedUsers", ONLINE_USERS);
 }
-
 
 // Sends tho the web that tha actual turn player selected to attack
 export const sendAttackSelectedToWeb = (io: Server):void => {
@@ -27,3 +27,7 @@ export const sendUsePotionSelectedToWeb = (io: Server):void => {
     io.emit("web-selectUsePotion");
 }
 
+// Sends the player data to server
+export const sendUserDataToWeb = (io: Server, player:Player):void => {
+    io.emit("web-sendUser", player);
+}
