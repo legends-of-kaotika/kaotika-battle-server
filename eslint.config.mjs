6c3e6e9837b2843a30a00f0dafd9fb6483ec0cf6
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-02-03 12:36:50 +0100

    [KBS-73][UPDATE] Eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,44 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react': react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
+      'react/jsx-first-prop-new-line': ['error', 'multiline'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'no-unused-vars': ['error', {
+        vars: 'all',
+        args: 'after-used',
+        caughtErrors: 'all',
+        ignoreRestSiblings: false,
+        reportUsedIgnorePattern: false
+      }],
+      'function-paren-newline': ['error', 'never'],
+    },
+  },
+)
--- src/sockets/emits/user.ts
+++ src/sockets/emits/user.ts
@@ -1,71 +1,71 @@
-import { Server, Socket } from "socket.io";
-import { ONLINE_USERS, webSocketId } from "../../game";
-import { Player } from "../../interfaces/Player";
-import { ASSING_TURN, CONNECTED_USERS, GAME_START, SEND_TIMER, UPDATE_PLAYER, WEB_SELECT_CURSE, WEB_SELECT_HEAL, WEB_SELECT_USE_POTION, WEB_SEND_USER, WEB_SET_SELECTED_PLAYER } from "../../constants/constants";
-import { returnLoyalsAndBetrayers } from "../../helpers/helper";
-import { DividedPlayers } from "../../interfaces/DividedPlayers";
-import { Modifier } from "../../interfaces/Modifier";
+import { Server, Socket } from 'socket.io';
+import { ONLINE_USERS, webSocketId } from '../../game';
+import { Player } from '../../interfaces/Player';
+import { ASSING_TURN, CONNECTED_USERS, GAME_START, SEND_TIMER, UPDATE_PLAYER, WEB_SELECT_CURSE, WEB_SELECT_HEAL, WEB_SELECT_USE_POTION, WEB_SEND_USER, WEB_SET_SELECTED_PLAYER } from '../../constants/constants';
+import { returnLoyalsAndBetrayers } from '../../helpers/helper';
+import { DividedPlayers } from '../../interfaces/DividedPlayers';
+import { Modifier } from '../../interfaces/Modifier';
 
 //sends an array with the connected users to web client on user connection
 export const sendConnectedUsersArrayToWeb = (io: Server):void => {
-    console.log('Emitting connectedUsers socket message with online user list to everyone.')
-    const dividedPlayers: DividedPlayers = returnLoyalsAndBetrayers();
-    io.to(webSocketId).emit(CONNECTED_USERS, dividedPlayers);
-}
+  console.log('Emitting connectedUsers socket message with online user list to everyone.');
+  const dividedPlayers: DividedPlayers = returnLoyalsAndBetrayers();
+  io.to(webSocketId).emit(CONNECTED_USERS, dividedPlayers);
+};
 
 export const sendConnectedUsersArrayToAll = (io: Server):void => {
-    console.log('Emitting connectedUsers socket message with online user list to everyone.')
-    const dividedPlayers: DividedPlayers = returnLoyalsAndBetrayers();    
-    io.emit(CONNECTED_USERS, dividedPlayers);
-}
+  console.log('Emitting connectedUsers socket message with online user list to everyone.');
+  const dividedPlayers: DividedPlayers = returnLoyalsAndBetrayers();    
+  io.emit(CONNECTED_USERS, dividedPlayers);
+};
 
 // Sends tho the web that tha actual turn player has selected a player
 export const sendSelectedPlayerIdToWeb = (io: Server, player: Player | undefined):void => {
-    if (player) {
-        io.to(webSocketId).emit(WEB_SET_SELECTED_PLAYER, player._id );
-    }
-}
+  if (player) {
+    io.to(webSocketId).emit(WEB_SET_SELECTED_PLAYER, player._id );
+  }
+};
 
 // Sends tho the web that tha actual turn player selected to heal
 export const sendHealSelectedToWeb = (io: Server):void => {
-    io.to(webSocketId).emit(WEB_SELECT_HEAL);
-}
+  io.to(webSocketId).emit(WEB_SELECT_HEAL);
+};
 
 // Sends tho the web that tha actual turn player selected to heal
 export const sendCurseSelectedToWeb = (io: Server):void => {
-    io.to(webSocketId).emit(WEB_SELECT_CURSE);
-}
+  io.to(webSocketId).emit(WEB_SELECT_CURSE);
+};
 
 // Sends tho the web that tha actual turn player selected to use a potion
 export const sendUsePotionSelectedToWeb = (io: Server):void => {
-    io.to(webSocketId).emit(WEB_SELECT_USE_POTION);
-}
+  io.to(webSocketId).emit(WEB_SELECT_USE_POTION);
+};
 
 // Sends the player data to server
 export const sendUserDataToWeb = (io: Server, player:Player):void => {
-    console.log(`Emitting web-sendUser socket message with ${player.name}'s player data to web.`)
-    io.to(webSocketId).emit(WEB_SEND_USER, player);
-}
+  console.log(`Emitting web-sendUser socket message with ${player.name}'s player data to web.`);
+  io.to(webSocketId).emit(WEB_SEND_USER, player);
+};
 
 // Sends the player data to server
 export const sendTimerDataToAll = (io: Server, timer:number):void => {
-    console.log(`Emitting send-timer socket message with turn time: ${timer} to all clients.`)
-    io.emit(SEND_TIMER, timer);
-}
+  console.log(`Emitting send-timer socket message with turn time: ${timer} to all clients.`);
+  io.emit(SEND_TIMER, timer);
+};
 
 // Sends the player data to server
 export const assingTurn = (io: Server, player:Player):void => {
-    console.log(`Emitting assing-turn socket message with ${player.name}'s player data to all devices to change turn.`)
-    io.emit(ASSING_TURN, player._id);
-}
+  console.log(`Emitting assing-turn socket message with ${player.name}'s player data to all devices to change turn.`);
+  io.emit(ASSING_TURN, player._id);
+};
 
 export const gameStartToAll = (io: Server):void => {
-    console.log(`Emitting gameStart socket message to all devices.`)
-    io.emit(GAME_START);
-}
+  console.log('Emitting gameStart socket message to all devices.');
+  io.emit(GAME_START);
+};
 
 // Sends the target players(id) with the attributes updated and the total damage
 export const sendUpdatedPlayerToAll = (io: Server, id:string, updatedAttributes:Modifier, totalDamage:number): void => {
-    console.log(`Emitting updatePlayer socket message with ${id} id, the total damage and updated attributes`);
-    io.emit(UPDATE_PLAYER, { _id: id, attributes: updatedAttributes, totalDamage: totalDamage})
-}
+  console.log(`Emitting updatePlayer socket message with ${id} id, the total damage and updated attributes`);
+  io.emit(UPDATE_PLAYER, { _id: id, attributes: updatedAttributes, totalDamage: totalDamage});
+};
