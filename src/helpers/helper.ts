c16a541329d563382a905834dbcebf54419ab65d
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-02-04 09:43:18 +0100

    [KBS-79][FIX] Change from .map to .find the functions on helpers

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,31 +1,22 @@
 import { Socket } from 'socket.io';
-import { ONLINE_USERS, currentPlayer, increaseTurn, setCurrentPlayer, turn } from '../game';
-import { Player } from '../interfaces/Player';
-import { MOBILE } from '../constants/constants';
-import { DividedPlayers } from '../interfaces/DividedPlayers';
-import { assingTurn } from '../sockets/emits/user';
 import { io } from '../..';
+import { MOBILE } from '../constants/constants';
+import { ONLINE_USERS, currentPlayer, increaseTurn, setCurrentPlayer, turn } from '../game';
+import { DividedPlayers } from '../interfaces/DividedPlayers';
+import { Player } from '../interfaces/Player';
+import { assingTurn } from '../sockets/emits/user';
 import { clearTimer, startTimer } from '../timer/timer';
 
 //returns a player searched by id
 export const findPlayerById = (_id: string): Player | undefined => {
-  ONLINE_USERS.map( player => {
-    if (player._id === _id) {
-      return player;
-    } 
-  });
-  return undefined;
+  const user = ONLINE_USERS.find((player)=> player._id === _id);
+  return user;
 };
 
 //returns a player searched by socketid
 export const findPlayerBySocketId = (id: string): Player | undefined => {
-
-  ONLINE_USERS.map( player => {
-    if (player.socketId === id) {
-      return player;
-    } 
-  });
-  return undefined;
+  const user = ONLINE_USERS.find((player)=> player.socketId === id);
+  return user;
 };
 
 //inserts socketId in the specific player of playerConnected[] global variable
@@ -50,12 +41,8 @@ export const removePlayerConnected = (socket: Socket, socketId: string): void =>
 
 //returns a player searched by email
 export const findPlayerByEmail = (email: string): Player | undefined => {
-  ONLINE_USERS.map( player => {
-    if (player.email === email) {
-      return player;
-    } 
-  });
-  return undefined;
+  const user = ONLINE_USERS.find((player)=> player.email === email);
+  return user;
 };
 
 //returns a boolean if a player is connected. searched by email
