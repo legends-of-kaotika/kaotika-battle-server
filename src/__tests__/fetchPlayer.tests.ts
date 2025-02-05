5a1649323ef42ac7be629dc68cc0081b09c5fe24
aitor <aitor.mendiburu@ikasle.aeg.eus>
2025-02-05 20:48:44 +0100

    [KBS-109][ADD] test handle errors of fetchPlayerController

--- src/__tests__/fetchPlayer.tests.ts
+++ src/__tests__/fetchPlayer.tests.ts
@@ -6,6 +6,7 @@ import { ONLINE_USERS_MOCK } from '../__mocks__/players';
 jest.mock('../services/playerService', () => ({
   initFetchPlayer: jest.fn()
 }));
+
 jest.mock('../game', () => ({
   isGameStarted: false,
   ONLINE_USERS: []
@@ -14,20 +15,17 @@ jest.mock('../game', () => ({
 describe('Player Controller - initFetchPlayerController', () => {
   let mockRequest: Partial<Request>;
   let mockResponse: Partial<Response>;
-  const mockStatus = jest.fn();
-  const mockSend = jest.fn();
-  
+
   beforeEach(() => {
     jest.clearAllMocks();
-    mockStatus.mockReturnThis();
 
     mockRequest = {
       params: { email: ONLINE_USERS_MOCK[0].email }
     };
-    
+
     mockResponse = {
-      status: mockStatus,
-      send: mockSend,
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
     };
   });
 
@@ -35,53 +33,64 @@ describe('Player Controller - initFetchPlayerController', () => {
     const mockPlayer = ONLINE_USERS_MOCK[0];
     (initFetchPlayer as jest.Mock).mockResolvedValue(mockPlayer);
 
+    await initFetchPlayerController(mockRequest as Request, mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.send).toHaveBeenCalledWith({ status: 'OK', data: mockPlayer });
+  });
+
+  it('should return 400 if email parameter is missing', async () => {
+    mockRequest.params = {};
+
     await initFetchPlayerController(mockRequest as Request,
       mockResponse as Response);
 
-    expect(mockStatus).toHaveBeenCalledWith(200);
-    expect(mockSend).toHaveBeenCalledWith({        
-      status: 'OK',
-      data: mockPlayer
+    expect(mockResponse.status).toHaveBeenCalledWith(400);
+    expect(mockResponse.send).toHaveBeenCalledWith({
+      status: 'FAILED',
+      data: { error: 'ERROR: Parameter :email cannot be empty' }
     });
   });
 
-  // it('should return 400 if email parameter is missing', async () => {
-  //   mockRequest.params = {};
+  it('should handle non-existent player', async () => {
+    (initFetchPlayer as jest.Mock).mockResolvedValue(null);
 
-  //   await initFetchPlayerController(mockRequest as Request,
-  //     mockResponse as Response);
+    await initFetchPlayerController(mockRequest as Request,
+    mockResponse as Response);
 
-  //   expect(mockResponse.status).toHaveBeenCalledWith(400);
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.send).toHaveBeenCalledWith({
+      message: 'Does not exist any player with this email'
+    });
+  });
+
+  it('should handle service errors', async () => {
+    const mockError = new Error('Service error');
+    (initFetchPlayer as jest.Mock).mockRejectedValue(mockError);
+
+    await initFetchPlayerController(mockRequest as Request,
+    mockResponse as Response);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockResponse.send).toHaveBeenCalledWith({
+      status: 'FAILED',
+      message: 'ERROR while making the petition:',
+      data: { error: mockError.message }
+    });
+  });
+
+  // it('should fail if the game has already started', async () => {
+  //   jest.mock('../game', () => ({
+  //     isGameStarted: true,
+  //     ONLINE_USERS: []
+  //   }));
+  
+  //   await initFetchPlayerController(mockRequest as Request, mockResponse as Response);
+  
+  //   expect(mockResponse.status).toHaveBeenCalledWith(403);
   //   expect(mockResponse.send).toHaveBeenCalledWith({
   //     status: 'FAILED',
-  //     data: { error: 'ERROR: Parameter :email cannot be empty' }
-  //   });
-  // });
-
-  // it('should handle non-existent player', async () => {
-  //   (initFetchPlayer as jest.Mock).mockResolvedValue(null);
-
-  //   await initFetchPlayerController(mockRequest as Request,
-  //     mockResponse as Response);
-
-  //   expect(mockResponse.status).toHaveBeenCalledWith(404);
-  //   expect(mockResponse.send).toHaveBeenCalledWith({
-  //     message: 'Does not exist any player with this email'
-  //   });
-  // });
-
-  // it('should handle service errors', async () => {
-  //   const mockError = new Error('Service error');
-  //   (initFetchPlayer as jest.Mock).mockRejectedValue(mockError);
-
-  //   await initFetchPlayerController(mockRequest as Request,
-  //     mockResponse as Response);
-
-  //   expect(mockResponse.status).toHaveBeenCalledWith(500);
-  //   expect(mockResponse.send).toHaveBeenCalledWith({
-  //     status: 'FAILED',
-  //     message: 'ERROR while making the petition:',
-  //     data: { error: mockError.message }
+  //     data: { error: 'Cannot join: Game is already in progress' }
   //   });
   // });
 });
