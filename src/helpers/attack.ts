8e395afe69c2596fd703afe0f6643aa4119464bd
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-07 13:52:15 +0100

    [KBS-120]update adjustAtributes function

--- src/helpers/attack.ts
+++ src/helpers/attack.ts
@@ -2,36 +2,19 @@ import { Player } from '../interfaces/Player';
 
 export const adjustAtributes = (player: Player): Player => {
 
-  //----ATTRIBUTES----
-  // Every attribute has to be rounded to integer
-  player.attributes.insanity = Math.round(player.attributes.insanity);
-  player.attributes.attack = Math.round(player.attributes.attack);
-  player.attributes.defense = Math.round(player.attributes.defense);
-  player.attributes.intelligence = Math.round(player.attributes.intelligence);
-  player.attributes.dexterity = Math.round(player.attributes.dexterity);
-  player.attributes.constitution = Math.round(player.attributes.constitution); 
-  player.attributes.charisma = Math.round(player.attributes.charisma);
-  player.attributes.strength = Math.round(player.attributes.strength);
-  player.attributes.hit_points = Math.round(player.attributes.hit_points);
-  player.attributes.magic_resistance = Math.round(player.attributes.magic_resistance);
-  player.attributes.CFP = Math.round(player.attributes.CFP);
-  player.attributes.BCFA = Math.round(player.attributes.BCFA);
-  player.attributes.resistance = Math.round(player.attributes.resistance);
-  //INS: min 1 - max 100
-  player.attributes.insanity = Math.max(1, Math.min(100, player.attributes.insanity));
- 
-  //Remaining attributes will have a minimum value of 0
-  player.attributes.defense = Math.max(0, player.attributes.defense);
-  player.attributes.intelligence = Math.max(0, player.attributes.intelligence);
-  player.attributes.dexterity = Math.max(0, player.attributes.dexterity);
-  player.attributes.constitution = Math.max(0, player.attributes.constitution); 
-  player.attributes.charisma = Math.max(0, player.attributes.charisma);
-  player.attributes.strength = Math.max(0, player.attributes.strength);
-  player.attributes.hit_points = Math.max(0, player.attributes.hit_points);
-  player.attributes.magic_resistance = Math.max(0, player.attributes.magic_resistance);
-  player.attributes.CFP = Math.max(0, player.attributes.CFP);
-  player.attributes.BCFA = Math.max(0, player.attributes.BCFA);
-  player.attributes.resistance = Math.max(0, player.attributes.resistance);
+  const attributes = Object.keys(player.attributes) as (keyof Player['attributes'])[];
+    
+  attributes.forEach((key) => {
+    // Every attribute has to be rounded to integer
+    player.attributes[key] = Math.round(player.attributes[key] as number);
+    // Remaining attributes will have a minimum value of 0
+    if (key !== 'insanity' && key !== 'attack') {
+      player.attributes[key] = Math.max(0, player.attributes[key] as number);
+      //INS: min 1 - max 100
+    }else if(key === 'insanity'){
+      player.attributes[key] = Math.max(1, Math.min(100, player.attributes[key] as number));
+    }
+  });
   return player;
 };
 
