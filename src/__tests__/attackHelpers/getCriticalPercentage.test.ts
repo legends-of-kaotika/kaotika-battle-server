415aedeaab1ed02ca7b9d56ddbbaf04b9d8309b4
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-02-07 13:31:53 +0100

    [KBS-125][UPDATE] Finish the getCriticalPercentage test.

--- src/__tests__/attackHelpers/getCriticalPercentage.test.ts
+++ src/__tests__/attackHelpers/getCriticalPercentage.test.ts
@@ -4,62 +4,33 @@ import { mock_CFP31, mock_CFP0 } from '../../__mocks__/attackMocks/getCriticalPe
 
 describe('getCriticalPercentage', () => {
   it('should return the correct critical percentage', () => {
-
-    // Arrange
     const player = mock_CFP31;
     const successPercentage = 20;
-    const CFP = 31;
-
-    // Act
     const result = getCriticalPercentage(player, successPercentage);
-
-    // Assert
-    expect(result).toBe(Math.ceil(CFP * successPercentage / 100));
-
+    expect(result).toBe(7);
   });
 
   it('should handle zero CFP correctly', () => {
-
-    // Arrange
     const player = mock_CFP0;
     const successPercentage = 20;
-
-    // Act
     const result = getCriticalPercentage(player, successPercentage);
-    
-    // Assert
     expect(result).toBe(0);
-
   });
 
-  it('should handle zero successPercentage correctly', () => {
-
-    // Arrange
+  it('should handle zero success percentage correctly', () => {
     const player = mock_CFP31;
     const successPercentage = 0;
-
-    // Act
     const result = getCriticalPercentage(player, successPercentage);
-
-    // Assert
     expect(result).toBe(0);
-
   });
 
 
   it('should always round up to the nearest integer', () => {
-
-    // Arrange
     const player = mock_CFP31;
     const successPercentage = 20;
-
-    // Act
     const result = getCriticalPercentage(player, successPercentage);
-
-    // Assert
-
-    // Math.ceil(31 * 20 / 100)
-    expect(result).toBe(result);
+    // 31 * 20 / 100 = 6.2 
+    expect(result).toBe(7);
   });
 
 
--- src/helpers/attack.ts
+++ src/helpers/attack.ts
@@ -2,6 +2,5 @@ import { Player } from '../interfaces/Player';
 
 
 export const getCriticalPercentage = (player: Player, successPercentage: number) => {
-  // Formula: CRIT% = CFP * SUCCESS% / 100 (result rounded up)
   return Math.ceil(player.attributes.CFP*successPercentage/100);
 };
