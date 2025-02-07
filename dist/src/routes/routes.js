c6e177d3eddedc08bd7f23136b48be65a22bbd05
aitor <aitor.mendiburu@ikasle.aeg.eus>
2025-02-07 13:36:38 +0100

    [KBS-112][ADD]dist folder in gitignore

--- dist/src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { initFetchPlayerController } from '../controllers/playerController.ts';
-const router = express.Router();
-export default router;
-router.get('/:email', (req, res) => {
-    initFetchPlayerController(req, res);
-});
