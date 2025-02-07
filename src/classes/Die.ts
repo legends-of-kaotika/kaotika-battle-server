912b69b1e90c0cc448fbe6283e1195e2895e8113
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-02-07 12:56:26 +0100

    [KBS-139][ADD]Die class tests

--- src/classes/Die.ts
+++ src/classes/Die.ts
@@ -4,21 +4,21 @@ export default class Die{
   private modifier: number;
 
   constructor
-  (num_faces: number, num_die: number, modifier: number){
+  (num_die: number, num_faces: number, modifier: number){
     this.num_faces = num_faces;
     this.num_die = num_die;
     this.modifier = modifier;
   }
 
-  public static create(num_faces: number, num_die: number, modifier: number) : Die{
-    return new Die(num_faces, num_die, modifier);
+  public static create(num_die: number, num_faces: number, modifier: number) : Die{
+    return new Die(num_die, num_faces, modifier);
   }
   
   public roll() : number{
     let roll = 0;
 
     for(let i = 0; i < this.num_die; i++){
-      roll += Math.floor(Math.random() * this.num_faces);
+      roll += Math.floor((Math.random() * this.num_faces) + 1);
     }
 
     return roll;
