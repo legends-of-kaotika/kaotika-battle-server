ced998669cdc98217bb6eb690188caed0ae63d90
aitor <aitor.mendiburu@ikasle.aeg.eus>
2025-02-10 23:37:48 +0100

    [KBS-131]getFumbleEffect method and test

--- src/helpers/fumble.ts
+++ src/helpers/fumble.ts
@@ -9,3 +9,17 @@ export const getCalculationFumbleDamage = (bcfa: number , weaponDieRoll: number 
 export const getCalculationFumblePercentile = (fumblePercentage: number, die100Roll: number): number => {
   return Math.ceil(100 * (die100Roll - fumblePercentage) / (100 - fumblePercentage));
 };
+
+export const getFumbleEffect = (fumblePercentile: number ): string => {
+  const effectsFumble = [
+    {max: 30, effect: 'slash'},
+    {max: 60, effect: 'fairytale'},
+    {max: 80, effect: 'hack'},
+    {max: Infinity, effect: 'scythe'}
+  ];
+  
+  const {effect} = effectsFumble.find((element)=> (fumblePercentile <= element.max))!;
+  return effect;
+};
+
+//-------------------------------------------------------------------------------------//
