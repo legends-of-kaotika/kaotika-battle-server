60512b2566f448961fb14c6ea711a9ed7501291d
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-02-14 12:19:29 +0100

    [KBS-223][FIX] getSuccessPercentage() and getFumblePercentage()

--- src/helpers/attack.ts
+++ src/helpers/attack.ts
@@ -56,11 +56,12 @@ export const getAttackRoll = (): number => {
 };
 
 export const getSuccessPercentage = (weaponBasePercentage: number, playerDexterity: number, playerInsanity: number): number => {
-  return weaponBasePercentage + Math.ceil(playerDexterity / 3) + playerInsanity;
+  const insMod = getInsanityModificator(playerInsanity);
+  return weaponBasePercentage + Math.ceil(playerDexterity / 3) + insMod;
 };
 
 export const getFumblePercentage = (playerCFP: number, successPercentage: number) => {
-  return Math.floor(100 - (100 - successPercentage) * playerCFP / 100);
+  return Math.floor((100 - (100 - successPercentage)) * playerCFP / 100);
 };
 export const getDefenseModificator = (value: number): number => {
   return getValueFromRule(DEFENSE_RULES, value);
