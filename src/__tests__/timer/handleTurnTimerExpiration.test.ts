92c53c274e7f82f082b74dc9670cc6036dffa33f
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-02-20 16:18:26 +0100

    [KBS-292][FIX]HandleTurnTimerExpiration

--- src/__tests__/timer/handleTurnTimerExpiration.test.ts
+++ src/__tests__/timer/handleTurnTimerExpiration.test.ts
@@ -1,8 +1,14 @@
 import { changeTurn } from '../../helpers/game.ts';
+import { sleep, logUnlessTesting } from '../../helpers/utils.ts';
 import { handleTurnTimerExpiration } from '../../timer/timer.ts';
 
 jest.mock('../../helpers/game', () => ({
   changeTurn: jest.fn(),
+}));
+
+jest.mock('../../helpers/utils', () => ({
+  sleep: jest.fn(),
+  logUnlessTesting: jest.fn(),
 }));
 
 describe('test handleTurnTimerExpiration', () => {
@@ -10,21 +16,25 @@ describe('test handleTurnTimerExpiration', () => {
     jest.resetAllMocks();
   });
 
-  it('should call changeTurn when turnTime is 0', () => {
+  it('should call changeTurn when turnTime is 0', async() => {
     // Arrange
     const turnTime = 0;   
     // act
-    handleTurnTimerExpiration(turnTime);
+    await handleTurnTimerExpiration(turnTime);
     // assert
+    expect(sleep).toHaveBeenCalledWith(5000);
     expect(changeTurn).toHaveBeenCalled();
+    expect(logUnlessTesting).toHaveBeenCalled();
   });
 
-  it('it should not call changeTurn when turnTime is not 0', () => {
+  it('it should not call changeTurn when turnTime is not 0', async() => {
     // Arrange
     const turnTime = 5;
     // act
-    handleTurnTimerExpiration(turnTime);
+    await handleTurnTimerExpiration(turnTime);
     // assert
+    expect(sleep).not.toHaveBeenCalled();
     expect(changeTurn).not.toHaveBeenCalled();
+    expect(logUnlessTesting).not.toHaveBeenCalled();
   });
 });
