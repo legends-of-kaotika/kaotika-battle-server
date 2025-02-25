f537e7b4a854e90328fd436be8dc7bcdbe0c1dad
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-02-25 13:23:55 +0100

    [KBS-322][UPDATE] Implement socket offs

--- src/sockets/listeners/globalHandlers.ts
+++ src/sockets/listeners/globalHandlers.ts
@@ -5,14 +5,21 @@ import { isGameStarted } from '../../game.ts';
 import { eachSideHasPlayers } from '../../helpers/game.ts';
 
 export const globalHandlers = (io: Server, socket: Socket): void => { 
-  //sends the new array of players on disconnect
+
   socket.on(DISCONNECT, async () => {
-    console.log(`disconnect socket message listened. Deleting user with socket: ${socket.id} from online users list.`);
+    
+    console.log(`Disconnect socket message listened. Deleting user with socket: ${socket.id} from online users list.`);
+    
+    // Remove from connected users.
     removePlayerConnected(socket, socket.id);
+
+    // Check if any team wins.
     if (isGameStarted) { 
       eachSideHasPlayers();
     }
+
+    // Clear all the listeners
+    socket.removeAllListeners();
   });
+
 };
-
-
