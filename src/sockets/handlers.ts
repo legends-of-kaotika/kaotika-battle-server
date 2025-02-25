e47139234048b7ffb4bc8d2b542940687f62a588
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-02-25 13:41:24 +0100

    [KBS-323][UPDATE]GlobalHandlers delete use io global instead of parameter

--- src/sockets/handlers.ts
+++ src/sockets/handlers.ts
@@ -1,12 +1,12 @@
 import { Server, Socket } from 'socket.io';
 
+import { globalHandlers } from './listeners/globalHandlers.ts';
 import { mobileUserHandlers } from './listeners/mobileHandlers/user.ts';
 import { webUserHandlers } from './listeners/webHandlers/user.ts';
-import { globalHandlers } from './listeners/globalHandlers.ts';
 
 export const socketHandlers = (io: Server, socket: Socket): void => {
   //Sockets used for BOTH parties: WEB & MOBILE
-  globalHandlers(io, socket);
+  globalHandlers(socket);
 
   //Sockets used for MOBILE
   mobileUserHandlers(io, socket);
--- src/sockets/listeners/globalHandlers.ts
+++ src/sockets/listeners/globalHandlers.ts
@@ -1,10 +1,10 @@
-import { Server, Socket } from 'socket.io';
+import { Socket } from 'socket.io';
 import { DISCONNECT } from '../../constants/sockets.ts';
-import { removePlayerConnected } from '../../helpers/player.ts';
 import { isGameStarted } from '../../game.ts';
 import { eachSideHasPlayers } from '../../helpers/game.ts';
+import { removePlayerConnected } from '../../helpers/player.ts';
 
-export const globalHandlers = (io: Server, socket: Socket): void => { 
+export const globalHandlers = (socket: Socket): void => { 
   //sends the new array of players on disconnect
   socket.on(DISCONNECT, async () => {
     console.log(`disconnect socket message listened. Deleting user with socket: ${socket.id} from online users list.`);
