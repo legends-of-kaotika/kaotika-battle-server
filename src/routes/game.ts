20519483db60389f018c103d8c755f03b750535d
ionpazoos <jon.pazos@ikasle.aeg.eus>
2025-02-25 10:04:28 +0100

    [KBS-318][DELETE]Services, routes, cotroller, using functions moved to player services

--- src/routes/game.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-import { getMissions } from '../controllers/game.ts';
-
-const router: express.Router = express.Router();
-export default router;
-
-router.get('/missions', (req: Request, res: Response) => {
-  getMissions(req, res);
-});
