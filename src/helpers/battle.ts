7a8f1c212e6e9ceb891693e3df8157e5614220b4
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-26 10:52:44 +0100

    [KBS-332]update playerdata

--- src/helpers/battle.ts
+++ src/helpers/battle.ts
@@ -2,6 +2,6 @@ import { BATTLES } from '../game.ts';
 import { Battle } from '../interfaces/Battles.ts';
 
 export const findBattleById = (_id: string): Battle | undefined => {
-  const battle = BATTLES.find((battle) => battle.id === _id);
+  const battle = BATTLES.find((battle) => battle._id === _id);
   return battle;
 };
