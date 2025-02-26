321e8d7f8ebea1cd3637c18a129dd39e48260bf8
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-26 12:25:18 +0100

    [KBS-338]When battle is created send to the person who emits the socket the status

--- src/sockets/emits/game.ts
+++ src/sockets/emits/game.ts
@@ -20,6 +20,12 @@ export const sendIsGameCreated = () : void => {
   logUnlessTesting(`emit the ${IS_GAME_CREATED} to all with isGameStarted: ${isGameCreated}`);
   io.emit(IS_GAME_CREATED, isGameCreated);
 };
+
+export const sendIsGameCreatedToEmiter = (socketId: string) : void => {
+  logUnlessTesting(`emit the ${IS_GAME_CREATED} to all with isGameStarted: ${isGameCreated}`);
+  io.to(socketId).emit(IS_GAME_CREATED, isGameCreated);
+};
+
 export const sendCreatedBattleToWeb = (battle: Battle | undefined): void => {
   io.to(webSocketId).emit(SOCKETS.WEB_CREATE_BATTLE, battle);
 };
