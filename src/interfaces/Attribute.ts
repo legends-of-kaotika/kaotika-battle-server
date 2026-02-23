7e5acb4731ae03d064b5b3d436d33e0bafb78c1c
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2026-02-23 12:15:40 +0100

    refactor(AS-64): update HP formula

--- src/interfaces/Attribute.ts
+++ src/interfaces/Attribute.ts
@@ -5,7 +5,7 @@ export interface Attribute {
   insanity: number;
   charisma: number;
   strength: number;
-  hit_points: number; //CON + DEX - INS/2
+  hit_points: number; // (LEVEL * 5) + CON + (DEX / 3) - (INS / 2) + (INT / 3) + (CHA * 2) + (STR / 3)
   attack: number; //STR - INS / 2
   defense: number; //DEX + CON + INT/2
   magic_resistance: number; //INT + CHA
